4a12c031c19af160e5dcfa45371606b6626b826a
Rawcossio <raulcossio166183@gmail.com>
2024-10-17 15:51:35 -0500

    ejercicio cajero hecho para practicar funciones

---
+++ script.js
@@ -0,0 +1,7 @@
+/*
+Simular el comportamiento de un cajero electronico con las siguientes funciones
+Consultar saldo, retirar dinero, consignar dinero (a cuenta propia) y transferir 
+dinero a cualquier cuenta
+Condiciones Debe existir una validación de inicio de sesión, solo se podra salir 
+del menu si el usuario lo decide
+*/
