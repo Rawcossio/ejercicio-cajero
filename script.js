b1f98884e3e3386dfee762a31e4cf49d3469aea5
Rawcossio <raulcossio166183@gmail.com>
2024-10-17 16:16:28 -0500

    se crea y valida que funcione el inicio de sesión

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ dinero a cualquier cuenta
 Condiciones Debe existir una validación de inicio de sesión, solo se podra salir 
 del menu si el usuario lo decide
 */
+let usuario='admin'
+let contrasena='admin'
+let inputUsuario=prompt('ingrese su usuario: ')
+let inputContrasena
+if(usuario==inputUsuario){
+    inputContrasena=prompt('ingrese su contraseña: ')
+    if(contrasena==inputContrasena){
+        console.log('Bienvenido'+inputUsuario)
+    }else{
+        console.log('Error credenciales')
+    }
+}else{
+    console.log('Usuario no existe en la base de datos')
+}
+
+
