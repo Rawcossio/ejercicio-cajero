b8dc439560108116a7df6b2dea6375e9b4e713e1
Rawcossio <raulcossio166183@gmail.com>
2024-10-22 14:40:36 -0500

    logica funciones cajero

--- script.js
+++ script.js
@@ -7,12 +7,73 @@ del menu si el usuario lo decide
 */
 let usuario='admin'
 let contrasena='admin'
-let inputUsuario=prompt('ingrese su usuario: ')
 let inputContrasena
+let inputUsuario
+let saldoCuenta=10000000
+
+
+function consultarSaldo(){
+console.log('su saldo es:'+saldoCuenta)
+}
+function retirarDinero(){
+    let valorTransaccion=Number(prompt('Ingrese el monto que desea retirar:'))
+    if(valorTransaccion<10000){
+        console.log('No se retirar menos de 10mil')
+    }else if(valorTransaccion>saldoCuenta){
+        console.log('Fondos insuficientes')
+    }else{
+        console.log('Hizo un retito por valor de:'+valorTransaccion)
+        saldoCuenta-=valorTransaccion
+        console.log('su nuevo saldo es:'+saldoCuenta)
+    }  
+}
+function transferirDinero(){
+    let valorTransaccion=Number(prompt('Ingrese el monto a transferir:'))
+    if(valorTransaccion>saldoCuenta){
+        console.log('Fondos inssuficientes')
+    }else{
+        console.log('Hizo una transaccion por valor de:'+valorTransaccion)
+        saldoCuenta-=valorTransaccion
+        console.log('su nuevo saldo es:'+saldoCuenta)
+    } 
+}
+function consignarDinero(){
+    let valorTransaccion=Number(prompt('Ingrese el monto a consignar:'))
+    if(valorTransaccion<10000){
+        console.log('No se puede consignar menos de 10mil')
+    }else{
+        console.log('Hizo una consignación por valor de:'+valorTransaccion)
+        saldoCuenta-=valorTransaccion
+        console.log('su nuevo saldo es:'+saldoCuenta)
+    } 
+}
+
+inputUsuario=prompt('ingrese su usuario: ')
 if(usuario==inputUsuario){
     inputContrasena=prompt('ingrese su contraseña: ')
     if(contrasena==inputContrasena){
         console.log('Bienvenido'+inputUsuario)
+        let repetir=true
+        while(repetir){
+            let opcion=Number(prompt('Seleccione:\n1-Consultar Saldo\n2-Retirar Dinero\n3-Transferir\n4-Consignar\n5-Salir'))
+            switch(opcion){
+                case 1:
+                    consultarSaldo()
+                break
+                case 2:
+                    retirarDinero()
+                break
+                case 3:
+                    transferirDinero()
+                break
+                case 4:
+                    consignarDinero()
+                break
+                case 5:
+                    repetir=false
+                break
+            }
+        }
     }else{
         console.log('Error credenciales')
     }
